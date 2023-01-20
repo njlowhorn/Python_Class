971210420252880af08fecac5b4aacbe38e0cbc5
njlowhorn <75708236+njlowhorn@users.noreply.github.com>
2023-01-19 19:01:04 -0500

    Create main.py

---
+++ Module1/ChapterThreeAssignment/main.py
@@ -0,0 +1,13 @@
+
+seconds_per_hour = 3600
+
+seconds_per_day = seconds_per_hour * 24
+
+hours_per_day_float = seconds_per_day / seconds_per_hour
+
+hours_per_day_int = seconds_per_day // seconds_per_hour
+
+print(hours_per_day_float)
+print(hours_per_day_int)
+
+# 3.6: Yes, both of the answers matched each other.
