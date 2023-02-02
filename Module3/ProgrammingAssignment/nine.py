927c0d7fad9994c25e48c2ced40c28574938a025
njlowhorn <75708236+njlowhorn@users.noreply.github.com>
2023-02-01 19:16:36 -0500

    Update nine.py

--- Module3/ProgrammingAssignment/nine.py
+++ Module3/ProgrammingAssignment/nine.py
@@ -1,21 +1,22 @@
 # Python V 3.9.0
 
-if __name__ == "__main__":
-
-    def good():
+def good():
         character_list = ["Harry", "Ron", "Hermione"]
         return character_list
 
-    def get_odds():
-        odds = []
-        for i in range(10):
-            if i % 2 != 0:
-                odds.append(i)
-        print(odds)
+def get_odds():
+    odds = []
+    for i in range(10):
+        if i % 2 != 0:
+            odds.append(i)
+    print(odds)
 
-        for item in odds:
-            if item == odds[2]:
-                print(odds[2])
+    for item in odds:
+        if item == odds[2]:
+            yield odds[2]
+
+if __name__ == "__main__":
 
     print(good())
-    get_odds()
+    for i in get_odds():
+        print(i)
